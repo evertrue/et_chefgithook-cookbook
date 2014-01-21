d89c5c95a1e885b82e702c7f98fc1c2ae2a1ad79
Eric Herot <eric.github@herot.com>
2014-01-21 17:23:46 -0500

    First rev

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+set['chefgithook']['knife']['validation_client_name'] = 'evertrue-validator'
+set['chefgithook']['s3']['bucket'] = 'ops.evertrue.com'
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'et_chefgithook'
+maintainer       'EverTrue, Inc.'
+maintainer_email 'eric.herot@evertrue.com'
+license          'All rights reserved'
+description      'Wrapper cookbook for chefgithook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.1'
+
+depends 'chefgithook', '= 1.0.1'

ad3d997bb4d2fa2bcb3304cdc5d5aa4924d9c802
Eric Herot <eric.github@herot.com>
2015-04-01 17:56:39 -0400

    Bump chefgithook 1.1.1

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Wrapper cookbook for chefgithook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
 
-depends 'chefgithook', '= 1.1.0'
+depends 'chefgithook', '= 1.1.1'

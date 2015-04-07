203f6f1d3d421cc4f598be167fa9cfd80faaeb9d
Eric Herot <eric.github@herot.com>
2015-04-07 16:01:45 -0400

    Pare down chef_repo attribute to just the github specific parts

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 set['chefgithook']['knife']['validation_client_name'] = 'evertrue-validator'
 set['chefgithook']['s3']['bucket'] = 'ops.evertrue.com'
-set['chefgithook']['chef_repo'] = 'git@github.com:evertrue/server-chef.git'
+set['chefgithook']['chef_repo'] = 'evertrue/server-chef'

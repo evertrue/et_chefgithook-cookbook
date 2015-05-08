59269557eaa16e7ae46755b8a0344f85757b764d
Eric Herot <eric.github@herot.com>
2015-05-08 17:37:31 -0400

    Version bump 2.0.4

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Wrapper cookbook for chefgithook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.3'
+version          '2.0.4'
 
 depends 'chefgithook', '~> 2.2'

f18c2fc21c1ece3f81e10007b20f5c2181c2a2c8
Eric Herot <eric.github@herot.com>
2016-03-29 17:43:37 -0400

    Set chef_server node attribute (because it is no longer in knife.rb)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
 set['chefgithook']['knife']['validation_client_name'] = 'evertrue-validator'
 set['chefgithook']['s3']['bucket'] = 'ops.evertrue.com'
 set['chefgithook']['chef_repo'] = 'evertrue/server-chef'
+set['chefgithook']['knife']['chef_server'] =
+  'https://api.opscode.com/organizations/evertrue'

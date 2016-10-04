6e9e1aaf6171bfe6964c79c93bf6b02480a6ff53
Eric Herot <eric.github@herot.com>
2016-10-04 15:05:04 -0400

    Put full repo names in repo dir hash

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,6 @@ set['chefgithook']['s3']['bucket'] = 'ops.evertrue.com'
 set['chefgithook']['knife']['chef_server'] =
   'https://api.opscode.com/organizations/evertrue'
 override['chefgithook']['chef_repo_dirs'] = {
-  'server-chef' => "#{node['chefgithook']['home']}/chef-updater/server-chef",
-  'unix-users' => "#{node['chefgithook']['home']}/chef-updater/unix-users"
+  'evertrue/server-chef' => "#{node['chefgithook']['home']}/chef-updater/server-chef",
+  'evertrue/unix-users' => "#{node['chefgithook']['home']}/chef-updater/unix-users"
 }
